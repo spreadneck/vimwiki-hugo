6bff8fef3d63c47e4243033c7b7328ab8582c39a
Nate <nate@spreadneck.com>
2025-07-19 13:30:32 -0500

    fix(release/0.1):

--- dotfiles/nvim/.config/nvim/lua/plugins/init.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/init.lua
@@ -1,46 +1,84 @@
-require("lazy").setup({{
-    "vimwiki/vimwiki",
-    init = function()
-        vim.g.vimwiki_list = {{
-            path = "~/vimwiki-hugo/content",
-            syntax = "markdown",
-            ext = ".md",
-            auto_export = 0,
-            links_space_char = "_",
-            template_path = "~/vimwiki-hugo/templates",
-            template_default = "default",
-            template_ext = ".md",
-            index = "_index"
-        }}
-        vim.g.vimwiki_diary_rel_path = "posts"
-        vim.g.vimwiki_diary_index = "_index"
-        vim.g.vimwiki_markdown_link_ext = 1
-        -- REMOVE: Lua-side templates, now handled from disk files per Vimwiki templates
-    end
-}, -- 🔧 Core dependencies
-{"nvim-lua/plenary.nvim"}, -- LSP support
-{"neovim/nvim-lspconfig"}, {"williamboman/mason.nvim"}, {"williamboman/mason-lspconfig.nvim"}, {
-    'plasticboy/vim-markdown',
-    branch = 'master',
-    require = {'godlygeek/tabular'}
-}, -- Formatters and linters
-{
-    "nvimtools/none-ls.nvim",
-    config = function()
-        local null_ls = require("null-ls")
-        null_ls.setup({
-            sources = {null_ls.builtins.formatting.prettier, null_ls.builtins.formatting.black,
-                       null_ls.builtins.formatting.isort, null_ls.builtins.formatting.stylua,
-                       null_ls.builtins.diagnostics.flake8, null_ls.builtins.diagnostics.markdownlint,
-                       null_ls.builtins.diagnostics.codespell}
-        })
-    end
-}, -- Treesitter
-{
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate"
-}}, {
+require("lazy").setup({
+    {
+        "vimwiki/vimwiki",
+        init = function()
+            vim.g.vimwiki_list = {
+                {
+                    path = "~/vimwiki-hugo/content",
+                    syntax = "markdown",
+                    ext = ".md",
+                    auto_export = 0,
+                    links_space_char = "_",
+                    template_path = "~/vimwiki-hugo/templates",
+                    template_default = "default",
+                    template_ext = ".md",
+                    index = "_index",
+                    diary_index = "_index",
+                    diary_rel_path = "posts",
+                    diary_header = "Journal",
+                }
+            }
+            -- vim.g.vimwiki_diary_rel_path = "posts"
+            -- vim.g.vimwiki_diary_index = "_index"
+            -- vim.g.vimwiki_markdown_link_ext = 1
+            -- REMOVE: Lua-side templates, now handled from disk files per Vimwiki templates
+        end
+    },
+    -- 🔧 Core dependencies
+    {"nvim-lua/plenary.nvim"}, -- LSP support
+    {"neovim/nvim-lspconfig"},
+    {"williamboman/mason.nvim"},
+    {"williamboman/mason-lspconfig.nvim"},
+    {
+        'plasticboy/vim-markdown',
+        branch = 'master',
+        require = {'godlygeek/tabular'}
+    },
+    -- Formatters and linters
+    {
+        "nvimtools/none-ls.nvim",
+        config = function()
+            local null_ls = require("null-ls")
+            null_ls.setup({
+                sources = {
+                    null_ls.builtins.formatting.prettier,
+                    null_ls.builtins.formatting.black,
+                    null_ls.builtins.formatting.isort,
+                    null_ls.builtins.formatting.stylua,
+                    null_ls.builtins.diagnostics.flake8,
+                    null_ls.builtins.diagnostics.markdownlint,
+                    null_ls.builtins.diagnostics.codespell
+                }
+            })
+        end
+    },
+    -- Treesitter
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate"
+    },
+    -- New plugins
+    {
+        "MeanderingProgrammer/render-markdown.nvim",
+        config = function()
+            require('render-markdown').setup({
+                file_types = { 'markdown', 'vimwiki' },
+            })
+        end
+    },
+    {
+        "tree-sitter-grammars/tree-sitter-markdown",
+        run = ":TSInstall markdown"
+    },
+    {
+        "latex-lsp/tree-sitter-latex",
+        run = ":TSInstall latex"
+    }
+}, {
     rocks = {
         enabled = false
     }
 })
+
+-- Register Treesitter language for Vimwiki
+vim.treesitter.language.register('markdown', 'vimwiki')
